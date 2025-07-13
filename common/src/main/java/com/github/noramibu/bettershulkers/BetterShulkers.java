45ec602ae4ae2c62348a4b3cde6149c74497ebb6
QPCrummer <qpcrummer@gmail.com>
2025-07-12 23:17:02 -0400

    Implement opening shulkers

--- common/src/main/java/com/github/noramibu/bettershulkers/BetterShulkers.java
+++ common/src/main/java/com/github/noramibu/bettershulkers/BetterShulkers.java
@@ -1,8 +1,16 @@
 package com.github.noramibu.bettershulkers;
 
 import com.github.noramibu.bettershulkers.command.ShulkerCommand;
+import com.github.noramibu.bettershulkers.interfaces.RemoteInventory;
+import com.github.noramibu.bettershulkers.interfaces.ShulkerViewer;
+import com.github.noramibu.bettershulkers.util.ShulkerUtil;
 import dev.architectury.event.events.common.CommandRegistrationEvent;
 import dev.architectury.event.events.common.InteractionEvent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.ShulkerBoxBlock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,15 +22,23 @@ public final class BetterShulkers {
     public static void init() {
         Config.init();
         // TODO Recipes
+        
         CommandRegistrationEvent.EVENT.register((commandDispatcher, commandBuildContext, commandSelection) -> {
             ShulkerCommand.register(commandDispatcher, commandBuildContext);
         });
 
         // TODO Item Component Modification
 
-        InteractionEvent.RIGHT_CLICK_ITEM.register((player, interactionHand) -> {
-            // TODO Open Shulkerbox UI
-            return null;
+        InteractionEvent.RIGHT_CLICK_ITEM.register((player, hand) -> {
+            ItemStack stack = player.getItemInHand(hand);
+            if (!player.level().isClientSide) {
+                if (ShulkerUtil.isShulkerBox(stack)) {
+                    ShulkerBoxBlock shulker = (ShulkerBoxBlock) ((BlockItem)stack.getItem()).getBlock();
+                    ((ShulkerViewer) player).setViewing(stack);
+                    ((RemoteInventory)shulker).openInventory((ServerPlayer) player, stack);
+                }
+            }
+            return InteractionResult.PASS;
         });
     }
 }
