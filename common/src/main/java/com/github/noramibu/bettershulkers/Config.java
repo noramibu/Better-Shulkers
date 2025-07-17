4fa8f8a5c6c5186693144c818dbfec9960d65001
QPCrummer <qpcrummer@gmail.com>
2025-07-17 19:38:16 -0400

    Improve config writing performance

--- common/src/main/java/com/github/noramibu/bettershulkers/Config.java
+++ common/src/main/java/com/github/noramibu/bettershulkers/Config.java
@@ -3,15 +3,17 @@ package com.github.noramibu.bettershulkers;
 import com.moandjiezana.toml.Toml;
 import dev.architectury.platform.Platform;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
 
 /**
  * Better Shulker's config
  */
 public final class Config {
-    private static File file;
-    private static Toml toml;
+    private static final Path CONFIG_PATH = Platform.getConfigFolder().resolve("bettershulkers.toml");
 
     public static boolean REQUIRE_PERMISSION_FOR_COMMAND;
     public static boolean REQUIRE_PERMISSION_FOR_RIGHT_CLICK_OPEN;
@@ -24,58 +26,55 @@ public final class Config {
      * The values from the config file are then loaded
      */
     public static void init() {
-        file = new File(Platform.getConfigFolder().toFile(), "bettershulkers.toml");
+        File file = CONFIG_PATH.toFile();
 
         if (!file.exists()) {
-            create();
+            create(file);
         }
 
-        load();
+        load(file);
     }
 
     /**
      * Reloads the config
      */
     public static void reload() {
-        load();
+        load(CONFIG_PATH.toFile());
     }
 
-    private static void create() {
-        try {
-            if (file.createNewFile()) {
-                try (java.io.FileWriter writer = new java.io.FileWriter(file)) {
-                    writer.write("# Better Shulkers Configuration\n\n");
+    private static void create(File file) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write("# Better Shulkers Configuration\n\n");
 
-                    writer.write("# Disables the main feature of this mod, which is shulkers retaining their inventory when broken.\n");
-                    writer.write("# NOTE: This does NOT disable the recipe to craft pickable shulkers.\n");
-                    writer.write("disable-pickup-feature-of-shulkers = false\n\n");
+            writer.write("# Disables the main feature of this mod, which is shulkers retaining their inventory when broken.\n");
+            writer.write("# NOTE: This does NOT disable the recipe to craft pickable shulkers.\n");
+            writer.write("disable-pickup-feature-of-shulkers = false\n\n");
 
-                    writer.write("# If true, a recipe is added to make shulkers pickable.\n");
-                    writer.write("# NOTE: This does NOT disable the pickup feature, only the recipe to craft them.\n");
-                    writer.write("add-recipe-for-pickable-shulker = true\n\n");
+            writer.write("# If true, a recipe is added to make shulkers pickable.\n");
+            writer.write("# NOTE: This does NOT disable the pickup feature, only the recipe to craft them.\n");
+            writer.write("add-recipe-for-pickable-shulker = true\n\n");
 
-                    writer.write("# If true, players can open shulker boxes by right-clicking them in hand.\n");
-                    writer.write("right-click-to-open-shulker = true\n\n");
+            writer.write("# If true, players can open shulker boxes by right-clicking them in hand.\n");
+            writer.write("right-click-to-open-shulker = true\n\n");
 
-                    writer.write("# If true, players will need permissions to use the /shulker command.\n");
-                    writer.write("require-permission-for-command = true\n\n");
+            writer.write("# If true, players will need permissions to use the /shulker command.\n");
+            writer.write("require-permission-for-command = true\n\n");
 
-                    writer.write("# If true, players will need 'bettershulkers.open' permission to open shulker boxes by right-clicking them.\n");
-                    writer.write("require-permission-for-right-click-open-shulker = false\n\n");
+            writer.write("# If true, players will need 'bettershulkers.open' permission to open shulker boxes by right-clicking them.\n");
+            writer.write("require-permission-for-right-click-open-shulker = false\n\n");
 
-                    writer.write("# --- Permission Nodes ---\n");
-                    writer.write("# bettershulkers.command.set   - Allows setting a shulker's material.\n");
-                    writer.write("# bettershulkers.command.reload - Allows reloading this configuration file.\n");
-                    writer.write("# bettershulkers.open          - Allows opening shulker boxes by right-clicking them in hand.\n");
-                }
-            }
+            writer.write("# --- Permission Nodes ---\n");
+            writer.write("# bettershulkers.command.set   - Allows setting a shulker's material.\n");
+            writer.write("# bettershulkers.command.reload - Allows reloading this configuration file.\n");
+            writer.write("# bettershulkers.open          - Allows opening shulker boxes by right-clicking them in hand.\n");
         } catch (IOException e) {
             BetterShulkers.LOGGER.error("Failed to create config: ", e);
+            throw new RuntimeException(e);
         }
     }
 
-    private static void load() {
-        toml = new Toml().read(file);
+    private static void load(File file) {
+        Toml toml = new Toml().read(file);
         REQUIRE_PERMISSION_FOR_COMMAND = toml.getBoolean("require-permission-for-command", true);
         REQUIRE_PERMISSION_FOR_RIGHT_CLICK_OPEN = toml.getBoolean("require-permission-for-right-click-open-shulker", false);
         RIGHT_CLICK_TO_OPEN_SHULKER = toml.getBoolean("right-click-to-open-shulker", true);
