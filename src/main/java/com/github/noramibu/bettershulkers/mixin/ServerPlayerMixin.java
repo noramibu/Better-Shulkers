0d3f600f72d2aeb99da9efc0470bd9e3d273277b
QPCrummer <qpcrummer@gmail.com>
2026-06-06 22:05:12 -0400

    Prevent desyncs when right-clicking shulkers

--- src/main/java/com/github/noramibu/bettershulkers/mixin/ServerPlayerMixin.java
+++ src/main/java/com/github/noramibu/bettershulkers/mixin/ServerPlayerMixin.java
@@ -9,13 +9,18 @@ import com.github.noramibu.bettershulkers.container.VirtualContainer;
 import com.github.noramibu.bettershulkers.container.VirtualContainerHolder;
 import com.github.noramibu.bettershulkers.container.VirtualShulkerBoxContainer;
 import com.mojang.authlib.GameProfile;
+import java.util.OptionalInt;
+import net.minecraft.network.protocol.game.ClientboundSetCursorItemPacket;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -24,6 +29,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin extends Player implements VirtualContainerHolder {
 
+    @Shadow
+    public ServerGamePacketListenerImpl connection;
     private VirtualShulkerBoxContainer virtualContainer;
 
     public ServerPlayerMixin(Level level, GameProfile gameProfile) {
@@ -44,6 +51,12 @@ public abstract class ServerPlayerMixin extends Player implements VirtualContain
             this.virtualContainer = null;
             ShulkerBoxUtils.playShulkerCloseSound((ServerPlayer) (Object) this);
         }
+    }
+
+    @Inject(method = "openMenu", at = @At("HEAD"))
+    private void bettershulkers$preventDesyncs(MenuProvider provider, CallbackInfoReturnable<OptionalInt> cir) {
+        // Try to prevent desyncs
+        this.connection.send(new ClientboundSetCursorItemPacket(this.containerMenu.getCarried()));
     }
 
     @Override
